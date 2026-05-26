d689023cc34dec00cb6dee48ebaed3561a31408f
WillzKing <stollba@yandex.ru>
2026-05-27 01:12:00 +0700

    Still refactoring

--- src/app_gui.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <atomic>
-#include "shared_structs.h"
-#include "database.h"
-#include "tile_manager.h"
-#include "map_renderer.h"
-using namespace std;
-void run_gui_thread(SystemStatus* shared_data, Database* db, DatabaseQueue* dbQueue, atomic<bool>* should_stop);
--- src/app_server.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <atomic>
-#include "shared_structs.h"
-#include "database.h"
-
-using namespace std;
-
-void run_server_thread(SystemStatus* shared_data, Database* db, DatabaseQueue* dbQueue, atomic<bool>* should_stop);
